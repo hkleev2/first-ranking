c40850ee73bec8e12e3d19e3f334186efa85e04c
hklee <>
2023-08-29 22:01:15 +0900

    Initial commit

---
+++ src/main/java/com/hklee/firstranking/FirstRankingApplication.java
@@ -0,0 +1,13 @@
+package com.hklee.firstranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hklee/firstranking/FirstRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hklee.firstranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
