df7ab538c793f1f50ce0e1a85a9cd490c659faf5
hkleev2 <hkleev2@gmail.com>
2023-09-04 23:10:00 +0900

    comment Pull requtests #2 반영

--- src/main/java/com/hklee/firstranking/service/comment/UpdateCommentService.java
+++ src/main/java/com/hklee/firstranking/service/comment/UpdateCommentService.java
@@ -18,24 +18,24 @@ public CommentDto update(Long id, UpdateCommentDto dto) throws RuntimeException 
         Comment comment = commentRepository.findById(id).orElseThrow(() -> new RuntimeException("id에 해당하는 댓글이 없습니다."));
 
         // 요청자(로그인 아이디)가 코멘트 작성자가 맞는지 확인
-        if (comment.isOwner(loginId)) {
-            comment.changeText(dto.getText());
-            return CommentDto.of(comment);
-        } else {
+        if (!comment.isOwner(loginId)) {
             throw new RuntimeException("코멘트 작성자 본인만 수정 가능 합니다.");
         }
+
+        comment.changeText(dto.getText());
+        return CommentDto.of(comment);
     }
 
 public CommentDto delete(Long id) throws RuntimeException {
         Comment comment = commentRepository.findById(id).orElseThrow(() -> new RuntimeException("id에 해당하는 댓글이 없습니다."));
 
         // 요청자(로그인 아이디)가 코멘트 작성자가 맞는지 확인
-        if (comment.isOwner(loginId)) {
-            comment.delete();
-            return CommentDto.of(comment);
-        } else {
+        if (!comment.isOwner(loginId)) {
             throw new RuntimeException("코멘트 작성자 본인만 삭제 가능 합니다.");
         }
+
+        comment.delete();
+        return CommentDto.of(comment);
     }
 
 }
